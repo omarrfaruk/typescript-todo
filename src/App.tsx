1ea9c547a211ce33d32215dd6224d9cef74e4f19
jankar-with-tarek <avirus170@gmail.com>
2022-09-05 16:10:58 +0600

    added delete option and finish

--- src/App.tsx
+++ src/App.tsx
@@ -28,10 +28,15 @@ const App: FC = () => {
     setTodo("");
   }
 
+  const deleteTodo = (thisTodoForDelete: string): void => {
+    const notDeletedItem = allTodo.filter(t => t.todo !== thisTodoForDelete)
+    setAllTodo(notDeletedItem)
+  }
+
 
   return (
     <div>
-      <div className='bg-lime-400 h-[30vh] flex items-center'>
+      <div className='bg-teal-600 h-[30vh] flex items-center'>
         <div className='flex flex-col mx-auto w-1/4 gap-2 '>
           <input
             className='border border-slate-500 focus outline-none py-1 px-2 rounded-md '
@@ -51,16 +56,16 @@ const App: FC = () => {
             onChange={handleChange}
           />
           <button
-            className='py-1 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
+            className='py-1 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
             onClick={handleSubmit}>ADD</button>
 
         </div>
       </div>
-      <div>
+      <div className='mt-4 h-[70vh]'>
         {
           allTodo.map((sTodo: ITodo, key: number) => {
             return (
-              <ShowTodo key={key} todo={sTodo} />
+              <ShowTodo key={key} todo={sTodo} updateTodo={deleteTodo} />
             )
           })
         }
--- src/ShowTodo.tsx
+++ src/ShowTodo.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { ITodo } from './interfaces/todoInterface';
 
 interface Props {
-    todo: ITodo
+    todo: ITodo,
+    updateTodo(todoName: string): void
 }
 
-const ShowTodo = ({ todo }: Props) => {
+const ShowTodo = ({ todo, updateTodo }: Props) => {
     return (
-        <div>
-            <h1>{todo.todo}</h1>
-            <h1>{todo.timeline}</h1>
+        <div className='flex justify-center mt-4'>
+            <div className='flex w-1/4'>
+                <h1 className='text-center bg-yellow-500 py-2 w-2/4 '>{todo.todo}</h1>
+                <h1 className='text-center bg-yellow-200 py-2 w-1/4'>{todo.timeline}</h1>
+                <button onClick={() => updateTodo(todo.todo)} className='w-1/4 bg-teal-600 py-2 text-white'>x</button>
+            </div>
         </div>
     );
 };
